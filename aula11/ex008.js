b395afd1fd3c15679965bd7c09abc4ddb827604d
Mari Sato <marisato19@gmail.com>
2022-02-01 18:10:39 -0300

    exercicios js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if (vel > 60) { //COndição simples
+    console.log(`Você ultrapassou a velocidade premitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'França'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Vocé é Brasileiro!')
+} else {
+    console.log('Vocé é Estrangeiro!')
+}
